f46bf35029c7e2b53d7535a8556bc3b9b0e1cd84
Matheus Gustavo Dickel <84946048+MatheussGd@users.noreply.github.com>
2021-08-19 21:06:07 -0300

    Add files via upload

---
+++ AULA004/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8"/>
+    <title>Meu primeiro exemplo php</title>
+    <style>
+        h2{
+            color: red;
+            font-size: 50px;
+
+        }
+    </style>
+</head>
+<body>
+    <h1>TESTANDO PHP</h1>
+
+    <?php
+
+        echo "<h2>OLA, MUNDO</h2>" ;
+
+    ?>
+
+</body>
+</html>
+</html>
